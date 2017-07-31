44b8c41463be20aee08ba9589a3c43f46d7e224a
a84058354 <30487623+a84058354@users.noreply.github.com>
2017-07-31 11:18:27 +0530

    Update AppIdentifiers.java

--- src/Utils/AppIdentifiers.java
+++ src/Utils/AppIdentifiers.java
@@ -8,6 +8,14 @@ public class AppIdentifiers {
 	public static String MessagePackage="com.android.mms";
 	public static String CameraPackage="com.android.camera";
 	public static String CameraActivity="com.android.camera/.Camera";
+        public static String ContactPackage="com.android.contacts";
+	public static String ContactActivity="activities.PeopleActivity";
+	public static String SettingsPackage="com.android.settings";
+	public static String SettingActivity="Settings";
+	public static String MusicPackage="com.miui.player";
+	public static String MusicActivity="ui.MusicBrowserActivity";
+	public static String DialerPackage="com.android.contacts";
+	public static String DialerActivity="activities.TwelveKeyDialer";
 	
 	
 }
